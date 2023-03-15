4344cbbb07929481189251f5cdd238bfd1b7c72e
artiecannv <thefiftharthur@gmail.com>
2023-03-14 20:42:38 -0600

    got a good start on about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,36 +1,45 @@
 import React, { useState } from "react";
+import Me from "../assets/me.jpg";
 
 const About = (props) => {
   const { contactSelected, setContactSelected } = props;
   console.log(contactSelected);
 
   return (
-    <div>
-      <section className="about-container">
+    <section className="about-container">
+      <div>
+        <h2>About Me:</h2>
         <div>
-          <h2>About Me</h2>
-          <p>
-            Hey! I'm Artie. I am a training coordinator currently, but I am
-            branching out my career path to involve{" "}
-          </p>
-          <p>Technologies I am profiecient with:</p>
-          <ul className="technologies">
-            <li>HTML5, CSS and JavaScript</li>
-            <li>React</li>
-            <li>GitHub</li>
-          </ul>
-          <p>Technologies I have been exposed to:</p>
-          <ul className="technologies">
-            <li>React</li>
-            <li>Node</li>
-            <li>Express</li>
-            <li>SQL/NoSQL</li>
-            <li>jQuery</li>
-          </ul>
+          <img src={Me} className="myPhoto" alt="myPhoto" />
         </div>
-      </section>
-      <button onClick={() => setContactSelected(true)}>Contact Me</button>
-    </div>
+        <p>
+          Hey! I'm Artie. I am a training coordinator currently, but I am
+          branching out my career path to involve Web Development.
+        </p>
+        <p>
+          I have found a passion for front-end development, I love to style and
+          build nice and responsive pages.
+        </p>
+        <h3>Technologies I am profiecient with:</h3>
+        <ul className="tech-prof">
+          <li>HTML5, CSS and JavaScript</li>
+          <li>React</li>
+          <li>GitHub</li>
+        </ul>
+        <h3>Technologies I have been exposed to:</h3>
+        <ul className="tech-learn">
+          <li>Node</li>
+          <li>Express</li>
+          <li>SQL/NoSQL</li>
+          <li>jQuery</li>
+        </ul>
+        <h3>Education:</h3>
+        <ul>
+          <li> University of Denver - Full Stack Coding Certificate</li>
+        </ul>
+        <button onClick={() => setContactSelected(true)}>Contact Me</button>
+      </div>
+    </section>
   );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,22 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to='/about'>About Me</Link>
-        </li>
-        <li>
-          <Link to ='/'>Projects</Link>
-        </li>
-        <li>
-          <Link to = '/contact'>Contact</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+  // return (
+  //   <nav>
+  //     <ul>
+  //       <li>
+  //         <Link to='/about'>About Me</Link>
+  //       </li>
+  //       <li>
+  //         <Link to ='/'>Projects</Link>
+  //       </li>
+  //       <li>
+  //         <Link to = '/contact'>Contact</Link>
+  //       </li>
+  //     </ul>
+  //   </nav>
+  // );
 };
 
 export default Navbar;
