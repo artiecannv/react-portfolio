722913b19af1f53898b482dfb056c854d4e2dc10
artiecannv <thefiftharthur@gmail.com>
2023-03-25 22:55:30 -0600

    finished contact form and general styling. just need to add a final project and im good to go

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,11 +8,14 @@ const Navbar = () => {
   return (
     <header className="text-white bg-gray-900 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="/" className="ml-3 text-xl hover:text-blue-400">
+        <div className="title-font font-medium text-white mb-4 md:mb-0">
+          <a
+            href="/"
+            className="ml-3 text-xl hover:text-blue-400 hover:underline"
+          >
             Arthur, the Fifth
           </a>
-        </a>
+        </div>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-1 flex items-center text-base justify-between">
           <a
             href="/projects"
@@ -26,6 +29,14 @@ const Navbar = () => {
             className="inline-flex items-center bg-gray-800 border-0 py-1 px-5 focus:outline-none hover:bg-gray-700 hover:text-blue-400 rounded text-base mt-4 md:mt-0"
           >
             Work with Me
+            <MailIcon className="w-4 h-4 ml-1" />
+          </a>
+          <a
+            href="https://docs.google.com/document/d/109DQehfUgqW_FyFYlUoVQbzJCaZR3DTM5gJ3Vp0mMwM/edit?usp=sharing"
+            target="_blank"
+            className="inline-flex items-center bg-gray-800 border-0 py-1 px-5 focus:outline-none hover:bg-gray-700 hover:text-red-400 rounded text-base mt-4 md:mt-0"
+          >
+            Resume
             <MailIcon className="w-4 h-4 ml-1" />
           </a>
         </nav>
--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,10 @@ import { projects } from "../data";
 
 const Project = () => {
   return (
-    <section id="projectSecton" className="text-gray-400 bg-gray-700 body-font">
+    <section
+      id="projectSection"
+      className="text-gray-400 bg-gray-700 body-font"
+    >
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
           {/* Imported icon from HeroIcons */}
